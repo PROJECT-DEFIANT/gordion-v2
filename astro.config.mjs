5be2a8690651d24cbddfe79d2ad4738b3a653338
PROJECT-DEFIANT <szymonszyszkowski@gmail.com>
2023-03-16 13:31:46 +0100

    targets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+// import browserslist from "browserslist";
+// import { resolveToEsbuildTarget } from "esbuild-plugin-browserslist";
 
 // https://astro.build/config
 export default defineConfig({
